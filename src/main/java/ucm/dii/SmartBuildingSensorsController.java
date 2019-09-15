8ad3d60b601171e79d32d76e921f02fa2dfddd0a
Jorge Gomez <jjgomez@ucm.es>
2019-09-15 22:23:46 +0200

    second version

---
+++ src/main/java/ucm/dii/SmartBuildingSensorsController.java
@@ -0,0 +1,29 @@
+package ucm.dii;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class SmartBuildingSensorsController {
+
+	private final String code1="micasa" ;
+	private final String code2="tucasa" ;
+	
+	@RequestMapping("/building/temp")
+	public double getBuildingTemperature() {
+		return Math.random()*40;
+	}
+
+	@RequestMapping("/house/temp/{id}")
+	public double getApartmentTemperature(@PathVariable("id") String id) {
+		if (id.equals(code))
+			return 20f*Math.random();
+		else 
+			if (id.equals(code2))
+				return  30f*Math.random();
+			return -1;
+	}
+
+
+}
